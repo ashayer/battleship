e7633d4525c6a8748483a6606095caace2d4167a
Aran Hayer <ashayer@users.noreply.github.com>
2022-12-04 17:52:00 -0800

    delete playwright

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,6 @@
-import type { DefaultUser } from 'next-auth';
+import type { DefaultUser } from "next-auth";
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface Session {
     user?: DefaultUser & {
       id: string | undefined;
