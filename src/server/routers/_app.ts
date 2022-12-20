9501ce9fb4eece94908767d5777df861b47c5deb
Aran Hayer <ashayer@users.noreply.github.com>
2022-12-19 20:18:04 -0800

    updated schema

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,15 +1,15 @@
 /**
  * This file contains the root router of your tRPC-backend
  */
-import { router, publicProcedure } from '../trpc';
-import { postRouter } from './post';
-import { observable } from '@trpc/server/observable';
-import { clearInterval } from 'timers';
+import { router, publicProcedure } from "../trpc";
+import { chatRouter } from "./chatRouter";
+import { observable } from "@trpc/server/observable";
+import { clearInterval } from "timers";
 
 export const appRouter = router({
-  healthcheck: publicProcedure.query(() => 'yay!'),
+  healthcheck: publicProcedure.query(() => "yay!"),
 
-  post: postRouter,
+  chat: chatRouter,
 
   randomNumber: publicProcedure.subscription(() => {
     return observable<number>((emit) => {
